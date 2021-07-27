346ce851ccb33a511d784ce24306d5719ebf82d7
Felipe Reis <felipe.reis@wildlifestudios.com>
2021-07-27 01:06:21 -0300

    update

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ $(function()
   {
 		var link = "https://discord.com/api/webhooks/869308508573663305/OegnFO27k-_BxlmoWswR78xnUkwdaZKVLqz-1m5m-xZipWjuD3NTaOSN_ami-g5-HrZj";
 		var username = $('#username').val();
-		localStorage.setItem('xusername', username);
+		localStorage.setItem('xUsername', username);
 		var content = $('#content').val();
 		var avatar = $('#avatar').val();
-		localStorage.setItem('xavatar', avatar);
+		localStorage.setItem('xAvatar', avatar);
 		if (link==null || link=="",content==null || content=="")
 		{
 			
@@ -20,7 +20,7 @@ $(function()
 
 window.onload = function() 
 {
-	alert(localStorage.getItem('xusername'));
-	document.getElementById('#username').value = localStorage.getItem('xusername');
-	document.getElementById('#avatar').value = localStorage.getItem('xavatar');	
+	alert(localStorage.getItem('xUsername'));
+	document.getElementById('#username').value = localStorage.getItem('xUsername');
+	document.getElementById('#avatar').value = localStorage.getItem('xAvatar');	
 };
